99eed4effee7eed3d33586036382bd39577e3f29
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-07-21 13:46:57 -0700

    reverse words in string

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        reverseWords("hello");
     }
 
+    public static String reverseWords(final String original) {
+//        return "hello";
+//        System.out.println(original);
+        return original;
 
-
-
-
-
-
+    }
 }
